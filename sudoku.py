e6630a1a86022460000f9965de4a6240af4bb165
eranda-ihalagedara <67923594+eranda-ihalagedara@users.noreply.github.com>
2023-12-16 13:08:44 +1100

    Implement solver with recursion

--- sudoku.py
+++ sudoku.py
@@ -1,19 +1,23 @@
 import numpy as np
 
 
-def solve_sudoku(puzzle):
-    puzzle_orig = puzzle.copy()
+def solve_sudoku(puzzle_orig, level=1):
+    print('\n'+(level-1)*'\t'+f'Level {level}:')
+    puzzle = puzzle_orig.copy()
     remaining_cells = np.sum(puzzle==0)
     solved_cells = 0
     return_puzzle = False
     iteration = 1
-    while not return_puzzle:
-        print(iteration)
-        iteration+=1
 
+    print('\t'*level+'Iterations:',end=' ')
+    
+    while not return_puzzle:
         if remaining_cells == 0:
             return_puzzle=True
             break
+
+        print(iteration, end=' ')
+        iteration+=1
             
         candidates = get_candidate_list(puzzle)
         puzzle = fill_candidates(puzzle, candidates)
@@ -21,7 +25,7 @@ def solve_sudoku(puzzle):
 
         if solved_cells > 0:
             remaining_cells -= solved_cells 
-            continue
+            # print('Candidate fill')
 
         else:
             candidates = refine_candidates(candidates)
@@ -31,36 +35,34 @@ def solve_sudoku(puzzle):
 
             if solved_cells > 0:
                 remaining_cells -= solved_cells 
+                # print('Refined candidates')
 
             else:
-                candidates, filled = fill_dual_cells(puzzle, candidates)
+                return_puzzle = True
 
-                if filled:
-                    puzzle = fill_candidates(puzzle, candidates)
-                    solved_cells = remaining_cells-np.sum(puzzle==0)
-                    remaining_cells -= solved_cells 
+    if remaining_cells > 0:
+        # Fill first vacant cell and pass into resursive function
+        candidates = get_candidate_list(puzzle)
+        i,j = get_branching_position(candidates)
 
-                else:
-                    candidates, filled = fill_triple_cells(puzzle, candidates)
-
-                    if filled:
-                        puzzle = fill_candidates(puzzle, candidates)
-                        solved_cells = remaining_cells-np.sum(puzzle==0)
-                        remaining_cells -= solved_cells 
-
-                    else :
-                        # candidates = fill_first_possibility(candidates)
-                        # puzzle = fill_candidates(puzzle, candidates)
-    
-                        # solved_cells = remaining_cells-np.sum(puzzle==0)
+        if candidates[i,j] is not None and len(candidates[i,j])>1:
+            for candidate in candidates[i,j]:                   
+                puzzle_copy = puzzle.copy()
+                puzzle_copy[i,j] = candidate
+                puzzle_reduced, reduced_cells = solve_sudoku(puzzle_copy, level+1)
+                
+                if reduced_cells == 0:
+                    puzzle = puzzle_reduced
+                    remaining_cells = 0
+                    break
             
-                        # if solved_cells > 0:
-                        #     remaining_cells -= solved_cells 
-    
-                        # else:
-                        return_puzzle = True
 
-    return puzzle
+    if remaining_cells == 0:
+        if level==1:
+            print('\nPuzzle solved')
+    else:
+        print('Dead end')
+    return puzzle, remaining_cells
 
     
 def get_candidate_list(puzzle):
@@ -70,7 +72,7 @@ def get_candidate_list(puzzle):
         for j in range(9):
             if puzzle[i,j] == 0:
                 candidates[i,j] = list(numbers - set(puzzle[i,:]) - set(puzzle[:,j]) - set(puzzle[(i//3)*3 : (i//3)*3+3,(j//3)*3 : (j//3)*3+3].flatten()))
-
+    # print(candidates[0,1], candidates[0,0], puzzle[0,0], puzzle[0,1])
     return candidates
 
 
@@ -80,7 +82,7 @@ def fill_candidates(puzzle, candidates):
         for j in range(9): 
             if candidates[i,j] is not None and len(candidates[i,j])==1:
                 filled_puzzle[i,j] = candidates[i,j][0]
-
+                candidates = get_candidate_list(filled_puzzle)
     return filled_puzzle
 
 
@@ -94,10 +96,19 @@ def refine_candidates(candidates):
                 col_candidates = get_col_candidates(candidates, i, j)
                 block_candidates = get_block_candidates(candidates, i, j)
                 
-                cleaned_list = list(set(candidates[i,j]) - set(row_candidates) - set(col_candidates) - set(block_candidates))
+                row_cleaned_list = list(set(candidates[i,j]) - set(row_candidates))
+                col_cleaned_list = list(set(candidates[i,j]) - set(col_candidates))
+                block_cleaned_list = list(set(candidates[i,j]) - set(block_candidates))
+                all_cleaned_list = list(set(candidates[i,j]) - set(row_candidates) - set(col_candidates) - set(block_candidates))
                 
-                if len(cleaned_list) > 0:
-                    refined_candidates[i,j] = cleaned_list
+                if len(row_cleaned_list) == 1:
+                    refined_candidates[i,j] = row_cleaned_list
+                elif len(col_cleaned_list) == 1:
+                    refined_candidates[i,j] = col_cleaned_list
+                elif len(block_cleaned_list) == 1:
+                    refined_candidates[i,j] = block_cleaned_list
+                elif len(all_cleaned_list) == 1:
+                    refined_candidates[i,j] = all_cleaned_list
 
     return refined_candidates
 
@@ -135,6 +146,24 @@ def get_block_candidates(candidates, i, j):
     return block
 
 
+def get_branching_position(candidates):
+    i,j = 0,0
+    position_found = False
+
+    for num_candidates in range(2,9):
+        for i in range(9):
+            for j in range(9):
+                if candidates[i,j] is not None and len(candidates[i,j])==num_candidates:
+                    position_found = True
+                    break
+            if position_found:
+                break
+        if position_found:
+                break
+            
+    return i,j
+
+
 def count_occurances(element, selection):
     count = 0
     # Loop through the array and check for elements
@@ -143,82 +172,3 @@ def count_occurances(element, selection):
         count += 1
     return count
 
-
-def fill_dual_cells(puzzle, candidates):
-    filled=False
-    
-    for i in range(9):
-        for j in range(9):
-            if candidates[i,j] is not None and len(candidates[i,j])==2:
-                # Check row for duplicates and uniqueness
-                dcount = count_occurances(candidates[i,j], candidates[i,:])
-                if dcount==2:
-                    rcd = get_row_candidates(candidates, i, j)
-                    if rcd.count(candidates[i,j][0]) == 1 and rcd.count(candidates[i,j][1]) == 1:
-                        candidates[i,j] = [candidates[i,j][0]]
-                        filled = True
-                        return candidates, filled
-
-                # Check column for duplicates and uniqueness
-                dcount = count_occurances(candidates[i,j], candidates[:,j])
-                if dcount==2:
-                    ccd = get_col_candidates(candidates, i, j)
-                    if ccd.count(candidates[i,j][0]) == 1 and ccd.count(candidates[i,j][1]) == 1:
-                        candidates[i,j] = [candidates[i,j][0]]
-                        filled = True
-                        return candidates, filled
-
-                # Check block for duplicates and uniqueness
-                dcount = count_occurances(candidates[i,j], candidates[(i//3)*3 : (i//3)*3+3,(j//3)*3 : (j//3)*3+3].flatten())
-                if dcount==2:
-                    bcd = get_block_candidates(candidates, i, j)
-                    if bcd.count(candidates[i,j][0]) == 1 and bcd.count(candidates[i,j][1]) == 1:
-                        candidates[i,j] = [candidates[i,j][0]]
-                        filled = True
-                        return candidates, filled
-
-    return candidates, filled
-
-
-def fill_first_possibility(candidates):
-    for i in range(9):
-        for j in range(9):
-            if candidates[i,j] is not None and len(candidates[i,j])==2:
-                candidates[i,j] = [candidates[i,j][0]]
-    return candidates
-
-
-def fill_triple_cells(puzzle, candidates):
-    filled=False
-    
-    for i in range(9):
-        for j in range(9):
-            if candidates[i,j] is not None and len(candidates[i,j])==3:
-                # Check row for duplicates and uniqueness
-                dcount = count_occurances(candidates[i,j], candidates[i,:])
-                if dcount==3:
-                    rcd = get_row_candidates(candidates, i, j)
-                    if rcd.count(candidates[i,j][0]) == 2 and rcd.count(candidates[i,j][1]) == 2 and rcd.count(candidates[i,j][2]) == 2:
-                        candidates[i,j] = [candidates[i,j][0]]
-                        filled = True
-                        return candidates, filled
-
-                # Check column for duplicates and uniqueness
-                dcount = count_occurances(candidates[i,j], candidates[:,j])
-                if dcount==3:
-                    ccd = get_col_candidates(candidates, i, j)
-                    if ccd.count(candidates[i,j][0]) == 2 and ccd.count(candidates[i,j][1]) == 2 and ccd.count(candidates[i,j][2]) == 2:
-                        candidates[i,j] = [candidates[i,j][0]]
-                        filled = True
-                        return candidates, filled
-
-                # Check block for duplicates and uniqueness
-                dcount = count_occurances(candidates[i,j], candidates[(i//3)*3 : (i//3)*3+3,(j//3)*3 : (j//3)*3+3].flatten())
-                if dcount==3:
-                    bcd = get_block_candidates(candidates, i, j)
-                    if bcd.count(candidates[i,j][0]) == 2 and bcd.count(candidates[i,j][1]) == 2 and bcd.count(candidates[i,j][2]) == 2:
-                        candidates[i,j] = [candidates[i,j][0]]
-                        filled = True
-                        return candidates, filled
-
-    return candidates, filled
